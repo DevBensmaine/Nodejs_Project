7eca094bd362f9ff41cf5a0a0b6b3637e792eddd
DevBensmaine <bensmaine.hassan@gmail.com>
2020-08-07 12:33:01 +0100

    add Page Not Found

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const path = require('path');
 
 const app = express();
 
@@ -18,7 +18,7 @@ app.use('/',(req,res,next) => {
 })
 
 app.use((req,res,next)  => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 
