9bb91ef6813fd81f41a704465c94cd379be90870
DevBensmaine <bensmaine.hassan@gmail.com>
2020-08-07 14:45:34 +0100

    add product

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,12 @@ const path =  require('path');
 const express = require("express");
 const router = express.Router();
 const routeRoot = require('../util/path');
+const adminData = require('./admin');
 
 
 router.get('/',(req, res, next) => {
 
-  // res.send("<h1>Hassan Bensmaine</h1>")
+  console.log("Shop",adminData.products)
   res.sendFile(path.join(routeRoot,'views','shop.html'))
 });
 
